409c81cce19baa9603ab560f828fa283310c4c8d
SaidBySolo <saidbysolo@gmail.com>
2020-05-06 18:03:05 +0900

    Update 0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="dbkrpy",
-    version="0.4.0",
+    version="0.4.1",
     license='MIT',
     author="Ryu ju heon",
     author_email="SaidBySolo@gmail.com",

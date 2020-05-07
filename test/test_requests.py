91e65ca4a262b0a4ea115f02dccd7dbd98d59d7d
SaidBySolo <33891531+SaidBySolo@users.noreply.github.com>
2020-05-07 21:42:15 +0900

    erase pass else

--- test/test_requests.py
+++ test/test_requests.py
@@ -13,28 +13,16 @@ async def checkall():
         await dbkrpy.CheckVote.get_response(token,userid)
     except Exception:
         checklist.append("CB")
-    else:
-        pass
-    
     try:
         await dbkrpy.GetById.get_response(botid)
     except Exception:
         checklist.append("GBI")
-    else:
-        pass
-
     try:
         await dbkrpy.Page.get_response(1)
     except Exception:
         checklist.append("P")
-    else:
-        pass
-    
     try:
         await dbkrpy.UpdateGuilds.post_guild_count(token,userid)
     except Exception:
         checklist.append("UG")
-    else:
-        pass
-
     assert(len(checklist) == 0)

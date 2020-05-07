23af9c1b2ea773535675bd9aab275a5b00e1df3b
SaidBySolo <33891531+SaidBySolo@users.noreply.github.com>
2020-05-07 21:24:01 +0900

    Feat:Requests Test

---
+++ test/test_requests.py
@@ -0,0 +1,39 @@
+import dbkrpy
+import os
+
+token = os.environ['token']
+botid = os.environ['bot_id']
+userid = os.environ['user_id']
+
+checklist=[]
+
+async def checkall():
+    try:
+        await dbkrpy.CheckVote.get_response(token,userid)
+    except Exception:
+        checklist.append("CB")
+    else:
+        pass
+    
+    try:
+        await dbkrpy.GetById.get_response(botid)
+    except Exception:
+        checklist.append("GBI")
+    else:
+        pass
+
+    try:
+        await dbkrpy.Page.get_response(1)
+    except Exception:
+        checklist.append("P")
+    else:
+        pass
+    
+    try:
+        await dbkrpy.UpdateGuilds.post_guild_count(token,userid)
+    except Exception:
+        checklist.append("UG")
+    else:
+        pass
+
+    assert(checklist == 0)

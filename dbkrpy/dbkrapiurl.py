5555c9073c957d98b0814382d608c6ed28fa153f
SaidBySolo <saidbysolo@gmail.com>
2020-05-09 00:28:32 +0900

    Typo

--- dbkrpy/dbkrapiurl.py
+++ dbkrpy/dbkrapiurl.py
@@ -2,7 +2,7 @@ mainurl = "https://api.koreanbots.cf"
 PostURL = {
     'dbkrpostguild': f'{mainurl}/bots/servers',
     'dbkrsearch': f'{mainurl}/bots/search',    
-    'dbkrgerbyid': f'{mainurl}/bots/get/',
+    'dbkrgetbyid': f'{mainurl}/bots/get/',
     'dbkrvote': f'{mainurl}/bots/voted/',
     'dbkrcategories': f'{mainurl}/bots/category/'
 }
